2fcd4b72ecf514ad41a81f2da0e756155bb13858
minnat <minnat5@mxnnxt.com>
2018-02-16 05:28:31 -0500

    Add files via upload

---
+++ addy.py
@@ -0,0 +1,15 @@
+import string
+import random
+
+times = int(input("Enter the number of times you would like to jig: "))
+addy1 = input("Enter Address 1: ")
+
+def addy():
+	size = 4
+	chars1 = string.ascii_uppercase + string.digits
+	chars2 = ''.join(random.choice(chars1) for _ in range(size))
+	print(chars2+" "+addy1)
+
+
+for i in range(times):
+	addy()
